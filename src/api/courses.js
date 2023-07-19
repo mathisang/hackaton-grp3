87124f202a97806f6dfb9591dbb447f438327dd2
Julien Dupont <julien.dupont2018@gmail.com>
2023-07-20 01:20:21 +0200

    feat: search bar

--- src/api/courses.js
+++ src/api/courses.js
@@ -2,5 +2,5 @@ import api from "./api";
 
 export const getCourses = (params) => api.axios.get('/classes', {params : {
     ...params,
-        populate: ['cover', 'ownerId', 'ownerId.picture'],
+        populate: ['cover', 'ownerId', 'ownerId.picture', 'category'],
     }}).then((response) => response.data.data);
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,11 @@
-import { Box, InputBase } from '@mui/material';
-import { APP_SECTION_VARIANT } from '../config';
+import {Box, InputBase} from '@mui/material';
+import {APP_SECTION_VARIANT} from '../config';
 import SearchIcon from '@mui/icons-material/Search';
-import { styled, alpha } from '@mui/material/styles';
+import {alpha, styled} from '@mui/material/styles';
+import React, {useEffect, useRef, useState} from "react";
+import debounce from 'lodash.debounce';
+import {getCourses} from "../../api/courses";
+import {useNavigate} from "react-router-dom";
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -10,8 +14,27 @@ const Search = styled('div')(({ theme }) => ({
   '&:hover': {
     backgroundColor: alpha(theme.palette.common.black, 0.12),
   },
-  width: '60%',
   height: 'fit-content'
+}));
+
+const SearchWrapper = styled('div')(({ theme }) => ({
+  //only bootom radius
+  position: 'relative',
+  width: '60%',
+  // width: '60%',
+}));
+
+const SearchResultsWrapper = styled('div')(({ theme }) => ({
+  //only bootom radius
+  borderRadius: '0 0 31px 31px',
+  bottom: 0,
+  transform: 'translateY(100%)',
+  width: '100%',
+  position: 'absolute',
+  paddingLeft: '32px',
+  backgroundColor: theme.palette.common.white,
+  zIndex: 1,
+  boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
@@ -37,13 +60,72 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
  * @component SearchBar
  */
 const SearchBar = ({ variant = APP_SECTION_VARIANT }) => {
+
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+  const navigate = useNavigate();
+
+  const fetchSearchResults = () => {
+    if(!search || search.length === 0) return setResults([]);
+
+    getCourses({
+      "sort": "category.name",
+      "filters": {
+        "$or": [
+          {
+            "name": {
+              "$containsi": search
+            }
+          },
+          {
+            "category": {
+              "name": {
+                "$containsi": search
+              }
+            }
+          }
+        ]
+      }
+    }).then((data) => setResults(data));
+  }
+
+  const debouncedSearch = useRef(debounce((query) => setSearch(query), 500)).current;
+
+  useEffect(() => {
+    fetchSearchResults();
+  }, [search]);
+
+  // Cleanup the debounced function on component unmount
+  useEffect(() => {
+    return () => {
+      debouncedSearch.cancel();
+    };
+  }, [debouncedSearch]);
+
   return (
-    <Search>
+    <SearchWrapper>
+    <Search sx={{
+      borderRadius: results.length ? '31px 31px 0 0' : '31px'
+    }}>
       <SearchIconWrapper>
         <SearchIcon color={'primary'} />
       </SearchIconWrapper>
-      <StyledInputBase placeholder="Rechercher..." inputProps={{ 'aria-label': 'search' }} />
+      <StyledInputBase placeholder="Rechercher..." inputProps={{ 'aria-label': 'search' }} onChange={({target}) => debouncedSearch(target.value)}/>
     </Search>
+      {results.length > 0 && (
+          <SearchResultsWrapper>
+            <Box sx={{padding: '16px'}}>
+              {results.map((result) => {
+                return (
+                  <Box onClick={() => navigate(`/courses/${result.id}`)} sx={{color: 'black', fontWeight: 'bold', fontSize: '1.2em', padding: 1, cursor: 'pointer'}}>
+                    #{result.attributes.category.data[0].attributes.name} - {result.attributes.name}
+                    </Box>
+                )
+              })}
+            </Box>
+          </SearchResultsWrapper>
+      )}
+    </SearchWrapper>
   );
 };
 
