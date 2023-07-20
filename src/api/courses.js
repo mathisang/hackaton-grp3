e7522ce03ce5f5ab9e9aab6cdc52258f5638c0b2
Julien Dupont <julien.dupont2018@gmail.com>
2023-07-20 08:17:01 +0200

    fix: course card ui + courses endpoint

--- src/api/courses.js
+++ src/api/courses.js
@@ -1,6 +1,6 @@
 import api from "./api";
 
-export const getCourses = (params) => api.axios.get('/classes', {params : {
+export const getCourses = (params) => api.axios.get('/courses', {params : {
     ...params,
         populate: ['cover', 'ownerId', 'ownerId.picture'],
     }}).then((response) => response.data.data);
